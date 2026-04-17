f801a66e1be7af668a056cd9ed1183d4769226ef
neves <neves7100@gmail.com>
2026-04-17 11:40:48 +0900

    fix build script

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+
+  export default defineConfig({
+    base: '/ff14market/',  // リポジトリ名に合わせる
+    ...
 })
